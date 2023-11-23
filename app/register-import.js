19b1cb3a8d235f54e3d32b8a400747f8a9fb8bfc
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-11-23 11:59:26 +0000

    Fix import tests

--- app/register-import.js
+++ app/register-import.js
@@ -1,7 +1,6 @@
 const readXlsxFile = require('read-excel-file/node')
 const registerMap = require('./schema/register-map')
 const { baseSchema, manualSchema } = require('./schema/register-schema')
-const { parse } = require('date-fns')
 
 const processRows = async (register, sheet, map, schema) => {
   const { rows } = await readXlsxFile(register, { sheet, map, dateFormat: 'dd/mm/yyyy' })
@@ -19,17 +18,10 @@ const processRows = async (register, sheet, map, schema) => {
       return errors.push({ rowNum, row, errors: result.errors.details })
     }
 
-    console.log(row)
-
     const person = row.person
     const dog = row.dog
 
-    person.dateOfBirth = parse(person.dateOfBirth, 'dd/MM/yyyy', new Date())
-    dog.dateOfBirth = parse(dog.dateOfBirth, 'dd/MM/yyyy', new Date())
-
-    console.log(person.dateOfBirth)
-
-    const key = `${person.lastName}^${person.postcode}^${person.dateOfBirth}`
+    const key = `${person.lastName}^${person.postcode}^${person.dateOfBirth.getDate()}`
 
     const value = registerMap.get(key) || { ...person, dogs: [] }
 
