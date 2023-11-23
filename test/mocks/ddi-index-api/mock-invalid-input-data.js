8922b4708366008269e140b78c3f501f62e6b676
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-11-23 13:52:50 +0000

    Add API config to helm

---
+++ test/mocks/ddi-index-api/mock-invalid-input-data.js
@@ -0,0 +1,26 @@
+const data = [{
+  firstName: 'Bill',
+  lastName: 'Test',
+  addressLine1: '319 test street',
+  townOrCity: 'Swansea',
+  country: 'Wales',
+  postcode: 'AA1 1AA',
+  dateOfBirth: '28/04/1970',
+  phoneNumber: 3333333333,
+  email: 'test@example.com',
+  dogs: [
+    {
+      name: 'Pepper',
+      dateOfBirth: '23/11/2022',
+      colour: 'White',
+      gender: 'Male',
+      insuranceStartDate: '11/11/2023',
+      neutered: 'Yes',
+      microchipped: 'Yes',
+      microchipNumber: '2134567891',
+      indexNumber: 1234
+    }
+  ]
+}]
+
+module.exports = data
